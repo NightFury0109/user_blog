66daed4b587bbbf69e8b68a9546f2210534e9b4f
NightFury0109 <nightfury0109@gmail.com>
2022-08-05 22:31:30 +0200

    [complete]: post funcs

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,17 +6,117 @@ const validatePostInput = require('../validation/post');
 exports.getPosts = async (req, res) => {
   try {
     const posts = await Post.find().sort({ date: -1 });
-    res.json(posts)
+    res.json(posts);
   } catch (error) {
-    res.status(404).json({ msg: 'No posts found' })
+    res.status(404).json({ msg: 'No posts found' });
   }
 }
 
-exports.getPosts = async (req, res) => {
+exports.getPost = async (req, res) => {
   try {
-    const posts = await Post.find().sort({ date: -1 });
-    res.json(posts)
+    const post = await Post.findById(req.params.id);
+    res.json(post);
   } catch (error) {
-    res.status(404).json({ msg: 'No posts found' })
+    res.status(404).json({ msg: 'No post found with that ID' });
+  }
+}
+
+exports.createPost = async (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    // If any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+
+  try {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    await newPost.save();
+    res.json({ msg: "Created successfully" });
+  } catch (error) {
+    res.status(404).json({ msg: error });
+  }
+}
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ notauthorized: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ msg: 'No post found' });
+  }
+}
+
+exports.comment = async (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    // If any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
+
+    // Save
+    await post.save();
+
+    res.json({ msg: "success" });
+  } catch (error) {
+    res.status(404).json({ msg: 'No post found' });
+  }
+}
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.comments.filter(
+        comment => comment._id.toString() === req.params.comment_id
+      ).length === 0
+    ) {
+      return res
+        .status(404)
+        .json({ msg: 'Comment does not exist' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments.map(item => item._id.toString()).indexOf(req.params.comment_id);
+
+    // Splice comment out of array
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json({ msg: "success" });
+  } catch (error) {
+    res.status(404).json({ msg: 'No post found' });
   }
 }
